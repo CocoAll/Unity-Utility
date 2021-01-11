4407494b34d91b6b49a04bc1a222a050b0c55f4a
CocoAll <corentin.all@gmail.com>
2021-01-11 13:23:23 +0100

    [Patter Observer] v1.0
    First version of the Pattern observer
    TODO :
    -Add comments

---
+++ Pattern Observer/SignalSender.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Signal Sender", menuName = "Scriptable Object/SignalSender", order = 0)]
+public class SignalSender : ScriptableObject
+{
+    public List<SignalListener> listeners = new List<SignalListener>();
+
+    public void Raise()
+    {
+        for(int i = listeners.Count -1; i >= 0; i--)
+        {
+            listeners[i].OnSignalRaised();
+        }
+    }
+
+    public void RegisterListener(SignalListener signalListener)
+    {
+        listeners.Add(signalListener);
+    }
+
+    public void UnregisterListener(SignalListener signalListener)
+    {
+        listeners.Remove(signalListener);
+    }
+}
